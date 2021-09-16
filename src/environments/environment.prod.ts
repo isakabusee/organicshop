ab1c87163b8666f99ef0b09826ee915ade9f06c4
Isaac Busee <58541303+isakabusee@users.noreply.github.com>
2021-09-15 17:42:17 -0700

    second commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAZIPg30ioDR-0gKXIQSz0268NOtn7pSzU",
+    authDomain: "organicshop-15c16.firebaseapp.com",
+    projectId: "organicshop-15c16",
+    storageBucket: "organicshop-15c16.appspot.com",
+    messagingSenderId: "1034205860954",
+    appId: "1:1034205860954:web:f5a3c38b9409ccbd073463",
+    measurementId: "G-QXV2J8BFCP"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAZIPg30ioDR-0gKXIQSz0268NOtn7pSzU",
+    authDomain: "organicshop-15c16.firebaseapp.com",
+    projectId: "organicshop-15c16",
+    storageBucket: "organicshop-15c16.appspot.com",
+    messagingSenderId: "1034205860954",
+    appId: "1:1034205860954:web:f5a3c38b9409ccbd073463",
+    measurementId: "G-QXV2J8BFCP"
+  }
 };
 
 /*
